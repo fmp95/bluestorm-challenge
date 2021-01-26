1eba40542cf59ae22b6d21376a56b21400850da3
fmp95 <plotze.frederico.social@gmail.com>
2021-01-26 03:29:20 -0300

    Create settings file

---
+++ settings.py
@@ -0,0 +1,6 @@
+from os.path import join
+
+DATABASE_FILENAME = "products.txt"
+DATABASE_FILE_PATH = join("files", DATABASE_FILENAME)
+WORD_FREQUENCY_FILENAME = "word_frequency.json"
+WORD_FREQUENCY_FILE_PATH = join("files", WORD_FREQUENCY_FILENAME)
